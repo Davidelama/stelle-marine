b98a77fc2b79c2d27d786553f1d8650970a6f7a3
Davidelama <davide.breoni@outlook.it>
2026-01-26 13:14:54 +0100

    Updated jobber.py and leo_download_stelle.sh

--- src/stelle/jobber.py
+++ src/stelle/jobber.py
@@ -6,10 +6,10 @@ from builder import logtimer
 
 def job_maker(details):
     
-    runtime = 5e6#1e10#5e6
-    dumptime = 2e4#1e8#1e4
+    runtime = 1e9#1e10#5e6
+    dumptime = 1e7#1e8#2e4
     dumptime_angle = 1e4
-    restime = 1e9
+    restime = 1e8
     
     queue="topolCPUQ"
     input_name="test_input.run"
@@ -49,7 +49,7 @@ def job_maker(details):
 
 sigma=15                    #diameter of bead in mm. This does not enter in the simulation, as it is automatically set to 1, just makes it easier to define the other parameters
 n_mol=1                     #number of molecules (not yet fully implemented)
-n_beads=[13]#[5,7,9,11,13,15]    #number of beads per arm
+n_beads=[5,7,9,11,13,15]    #number of beads per arm
 r_core=20/(2*sigma)         #core radius (units of bead diameters)
 n_restarts=0                #number of times simulation is automatically restarted on cluster
 gh=0                        #molecules do not interact with each other (not yet fully implemented)
--- src/stelle/leo_download_stelle.sh
+++ src/stelle/leo_download_stelle.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-ns=(013) # (090)#
-fs=(006)  #(010 030 060 100 150) # (020 060 120 200 300)#
+ns=(005 007 009 011 013 015) # (090)#
+fs=(003 004 005 006)  #(010 030 060 100 150) # (020 060 120 200 300)#
 rcs=(0.67)
-ris=(4.0 5.0 6.0 7.0 8.0 9.0 10.0 12.0 14.0 16.0 18.0 20.0)
-mols=(002) #008
-suffixes=("_mols002_pe2.0_bro1.0")
+ris=(4.0) # (4.0 5.0 6.0 7.0 8.0 9.0 10.0 12.0 14.0 16.0 18.0 20.0)
+mols=(001) #008
+suffixes=("_pe2.3_rf10.0_Dt0.010_Dr0.800_gm100")
 for n in "${ns[@]}";
 do
 for f in "${fs[@]}";
@@ -18,7 +18,7 @@ do
 # for seed in "${seeds[@]}";
 # do
 
-name=mar_n"$n"_f"$f"_rc"$rc"_rb1.13_rcb0.57"$suffix"_ri"$ri"; #_mols"$mols"
+name=mar_n"$n"_f"$f"_rc"$rc"_rb1.13_rcb0.57"$suffix"; #_mols"$mols"_ri"$ri"
 echo downloading $name;
 #mkdir ../../data/01_raw/clicks
 scp -i ~/.ssh/id_cineca -r dbreoni0@login.leonardo.cineca.it:~/stelle-marine/data/01_raw/stelle/"$name" ../../data/01_raw/stelle/
